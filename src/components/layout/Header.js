774a22099d0ba4340019050bebcb8dd5c5dffb84
manliot <manliot@uninorte.edu.co>
2021-05-28 19:12:47 -0500

    fix: backgroud image changed

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,7 +3,6 @@ import Logo from '../../assets/Logo-complete.png'
 import styles from '../../css/componets/layout/Header.module.css'
 import Cumbiambera from '../../assets/cumbiambera.png'
 const Header = ({ refe }) => {
-    console.log(refe)
     return (
         <header className={styles['header']}>
             <div className={styles['header-message']}>
@@ -11,7 +10,7 @@ const Header = ({ refe }) => {
                 <img ref={refe} className={styles['logo']} src={Logo} alt='Logo de curramba' />
             </div>
             <div className={styles['header-woman']}>
-                <img  src={Cumbiambera} alt='Mujer' />
+                <img src={Cumbiambera} alt='Mujer' />
             </div>
         </header>
     )

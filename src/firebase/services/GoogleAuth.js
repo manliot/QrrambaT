7db7745ca123de83c033d6477188f25ecca0e535
manlio <manliot@uninorte.edu.co>
2021-05-29 18:39:50 -0500

    firebase config and goole auth popup

---
+++ src/firebase/services/GoogleAuth.js
@@ -0,0 +1,43 @@
+
+import { auth, firebase } from '../firebase_config'
+
+const GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export const LoginWithGoogle = async () => {
+
+    const user = await new Promise(async (resolve, reject) => {
+        await firebase.auth().signInWithPopup(GoogleAuthProvider)
+            .then((result) => {                
+                const credential = result.credential;
+
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+
+                resolve({ user, token });
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                const credential = error.credential;
+
+                console.log({ errorCode, errorMessage, email, credential });
+                reject({ errorCode, errorMessage, email, credential });
+            });
+    })
+    return user;
+}
+
+
+export const Logout = async () => {
+    await auth().signOut()
+        .then(() => {
+            console.log("Salio exitosamente")
+        }).catch((error) => {
+            console.log(error)
+        });
+}

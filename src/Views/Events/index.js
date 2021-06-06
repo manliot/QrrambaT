1ee041165750c14dd07b6bfd59f485c054bb18fe
Daniel Donado <danieldoav@gmail.com>
2021-06-06 12:17:16 -0500

    Event Details done

--- src/Views/Events/index.js
+++ src/Views/Events/index.js
@@ -1,37 +1,45 @@
 import React, { useEffect, useState } from "react";
 import SearchBar from "../../components/common/SearchBar";
 import Navbar from "../../components/layout/NavBar";
-import { getAllEvents } from '../../firebase/services/Firestorage'
+import { Link } from "react-router-dom";
+import { getAllEvents } from "../../firebase/services/Firestorage";
 import styles from "../../css/Views/Events.module.css";
 import CardEvent from "../../components/common/Card_event";
-import LoadinC from '../../components/common/Loading'
+import LoadinC from "../../components/common/Loading";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import { useInView } from "react-intersection-observer";
 
-const Events = () => {
-  const [events, setEvents] = useState(null)
-  const [events_filter, setEvents_filter] = useState(null)
-  const [busquedaTxt, setBusquedaTxt] = useState('')
+const Events = (props) => {
+  const [events, setEvents] = useState(null);
+  const [events_filter, setEvents_filter] = useState(null);
+  const [busquedaTxt, setBusquedaTxt] = useState("");
   useEffect(() => {
     const fetchAllplaces = async () => {
-      const events_ = await getAllEvents()
-      setEvents(events_)
-      setEvents_filter(events_)
-    }
-    fetchAllplaces()
-  }, [])
+      const events_ = await getAllEvents();
+      setEvents(events_);
+      setEvents_filter(events_);
+    };
+    fetchAllplaces();
+  }, []);
 
   const filter_events = (filter_text) => {
     /* setUpdating(true) */
-    setBusquedaTxt(filter_text)
-    if (filter_text === '') {
-      setEvents_filter(events)
+    setBusquedaTxt(filter_text);
+    if (filter_text === "") {
+      setEvents_filter(events);
     } else {
-      const filter_result = events.filter(event => event.name.toLowerCase().includes(filter_text.toLowerCase()))
-      setEvents_filter(filter_result)
+      const filter_result = events.filter((event) =>
+        event.name.toLowerCase().includes(filter_text.toLowerCase())
+      );
+      setEvents_filter(filter_result);
     }
     /* setUpdating(false) */
+  };
+
+  const openDetails = (evento) => {
+    props.history.push('/event-details',{Event:evento})
+    console.log(evento)
   }
 
   const { ref, inView /* entry */ } = useInView({
@@ -41,18 +49,29 @@ const Events = () => {
 
   if (!events || !events_filter) {
     return (
-      <div className="vh-100 vw-100 " >
+      <div className="vh-100 vw-100 ">
         <LoadinC />
       </div>
-    )
+    );
   }
 
   return (
     <div className={styles["container"]}>
-      <Navbar Func={filter_events} valueSearchVar={busquedaTxt} selected={2} type={`${inView ? "" : "search"}`} />
+      <Navbar
+        Func={filter_events}
+        valueSearchVar={busquedaTxt}
+        selected={2}
+        type={`${inView ? "" : "search"}`}
+      />
       <header className={styles["header"]}>
         <div ref={ref} className={styles["search-container"]}>
-          {inView && <SearchBar Func={filter_events} valueTxt={busquedaTxt} searchTxt="Que estas buscando" />}
+          {inView && (
+            <SearchBar
+              Func={filter_events}
+              valueTxt={busquedaTxt}
+              searchTxt="Que estas buscando"
+            />
+          )}
         </div>
       </header>
       <div className={styles["container"]}>
@@ -60,10 +79,13 @@ const Events = () => {
           <h2>Eventos Culturales</h2>
         </div>
         <div className={styles["three-columns"]}>
-          {
-            events_filter.map(((evento, index) => {
-              return (
-                <section key={`${evento.creador}+ ${index}`} className={styles["column"]}>
+          {events_filter.map((evento, index) => {
+            return (
+              <div className={styles["column"]} onClick={() => {openDetails(evento)}}>
+                <section
+                  key={`${evento.creador}+ ${index}`}
+                  
+                >
                   <CardEvent
                     title={evento.name}
                     time={evento.hour}
@@ -71,9 +93,9 @@ const Events = () => {
                     url_img={evento.photoURL}
                   />
                 </section>
-              )
-            }))
-          }
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
